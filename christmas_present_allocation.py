120f13e6e74896b6cac87e555e84df76cfbdfecf
Philipp <philipp.panhey@bo-bremen.de>
2019-11-29 12:49:17 +0100

    writes current allocation to file

--- christmas_present_allocation.py
+++ christmas_present_allocation.py
@@ -35,14 +35,20 @@ def getConfig(config_key):
         return json.loads(file.read().replace('\n', ''))[config_key]
 
 
+def writeFile(file_name, string):
+    filehandle = open(file_name, 'w')
+    filehandle.write(string)
+    filehandle.close()
+
+
 # script start
 participants_emails = getConfig('participants_emails')
 participant_names_shuffled = sorted(
     list(participants_emails.keys()), key=lambda k: random.random())
 
-print(participant_names_shuffled)
+writeFile('present_allocation.json', json.dumps(participant_names_shuffled))
 
 for idx, participant_name in enumerate(participant_names_shuffled):
-    message = 'Hallo ' + participant_name + ',\n\n' + 'dieses jahr beschenkst du ' + participant_names_shuffled[(idx + 1) % len(
-        participant_names_shuffled)] + '.\n\n\n ps: die auslosung und das senden der mail wurde mit einem selbstgeschriebenen python script realisiert, schau hier:'
-    sendMail(message, 'philipp.panhey@gmail.com')
+    message = 'hallo ' + participant_name + ',\n es weihnachtet sehr.\n' + 'dieses jahr beschenkst du ' + participant_names_shuffled[(idx + 1) % len(
+        participant_names_shuffled)] + '.\n\nbis naechstes jahr,\ndein python script.\n\n\n ps: es weiss niemand, wer wem was schenkt, denn die auslosung wurde ueber ein python script realisiert. wenn du sehen willst wie das funktioniert, schaue hier: \n https://github.com/phpanhey/secret_santa_allocation/blob/master/christmas_present_allocation.py'
+    sendMail(message, participants_emails[participant_name])
